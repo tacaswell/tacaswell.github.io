922caadc4bb2320d4ca2a97266d583367d4561dc
Thomas A Caswell <tcaswell@gmail.com>
2022-05-07 14:09:31 -0400

    Add some configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,10 +25,63 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
-FORMATTED_FIELDS = ['summary'] #, "title"]
+FORMATTED_FIELDS = ['summary']
 
 DEFAULT_METADATA = {
     'status': 'draft',
+}
+
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+#     ('GitHub', 'https://github.com/tacaswell'),
+)
+
+# example pagination pattern
+PAGINATION_PATTERNS = (
+    (1, '{url}', '{save_as}'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.extra': {},
+        'markdown.extensions.admonition': {},
+        'markdown.extensions.codehilite': {
+            'css_class': 'highlight',
+            'guess_lange': False
+        },
+        'markdown.extensions.meta': {},
+        'smarty' : {
+            'smart_angled_quotes' : 'true'
+        },
+        'markdown.extensions.toc': {
+            # 'permalink': 'true',
+        },
+    },
+    'output_format': 'html5'
 }
